02f032d8ff35047861bbcaa2f167ba8dd378fb06
darmawguna <komangagus642@gmail.com>
2023-11-19 14:40:12 +0800

    memperbaiki component

---
+++ resources/views/backpage/base/end.blade.php
@@ -0,0 +1,8 @@
+<!-- script -->
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script src="js/scripts.js"></script>
+@yield('addon-script')
+<!-- end script -->
+
+</body>
+</html>
---
+++ resources/views/backpage/index.blade.php
@@ -0,0 +1,26 @@
+@include('backpage.base.start')
+
+
+@include('backpage.base.navbar')
+
+
+<!-- strat wrapper -->
+<div class="h-screen flex flex-row flex-wrap">
+  
+  @include('backpage.base.sidebar')
+
+  <!-- start content -->
+  <div class="bg-gray-100 flex-1 p-6 md:mt-16"> 
+
+        
+
+  </div>
+  <!-- end content -->
+
+</div>
+<!-- end wrapper -->
+
+@include('backpage.base.end')
+
+
+
