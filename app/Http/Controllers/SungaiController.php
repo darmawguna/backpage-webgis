687c0ab1a8e808e44b4e47fcb1b8428c721fce1d
darmawguna <komangagus642@gmail.com>
2023-11-19 09:43:23 +0800

    membuat backpage webgis

---
+++ app/Http/Controllers/SungaiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sungai;
+use App\Http\Requests\StoreSungaiRequest;
+use App\Http\Requests\UpdateSungaiRequest;
+
+class SungaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('sungai');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSungaiRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSungaiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sungai  $sungai
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sungai $sungai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sungai  $sungai
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sungai $sungai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSungaiRequest  $request
+     * @param  \App\Models\Sungai  $sungai
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSungaiRequest $request, Sungai $sungai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sungai  $sungai
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sungai $sungai)
+    {
+        //
+    }
+}
